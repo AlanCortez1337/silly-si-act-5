2f17e8b1ed04952a1cbf26b27cca727b607e8961
Alan Cortez <78040578+AlanCortez1337@users.noreply.github.com>
2024-02-14 00:47:58 +0000

    added more starter code

--- main.cpp
+++ main.cpp
@@ -4,29 +4,22 @@ int main()
 {
     CoolerVector<int> myVector{};
 
-    // myVector.push_back(13);
+    myVector.push_back(13);
     myVector.pop_back();
-    // myVector.push_back(24);
-    // myVector.push_back(92);
-    // myVector.pop_back();
-    // myVector.push_back(74);
-    // myVector.push_back(34);
-    // myVector.push_back(75);
-    // myVector.pop_back();
-    // myVector.pop_back();
-    // myVector.push_back(75);
-    // myVector.push_back(123);
-    // myVector.push_back(17);
+    myVector.push_back(24);
+    myVector.push_back(92);
+    myVector.pop_back();
+    myVector.push_back(74);
+    myVector.push_back(34);
+    myVector.push_back(75);
+    myVector.pop_back();
+    myVector.pop_back();
+    myVector.push_back(75);
+    myVector.push_back(123);
+    myVector.push_back(17);
 
     // IGNORE: this is for later if we have time:>
-    // myVector.push_back(13);
-    // myVector.push_back(34);
-    // myVector.push_back(65);
-    // myVector.push_back(75);
-    // myVector.push_back(77);
-    // myVector.push_back(85);
-    // myVector.push_back(99);
-    // myVector.push_back(100);
+    // CoolerVector<double> preSortedVector({1,2,3,4,5,6,7,8,9,10,11,12,14}) 
 
     myVector.display();
 
--- vector.hpp
+++ vector.hpp
@@ -6,41 +6,48 @@ template <typename T>
 class CoolerVector
 {
 public:
-    CoolerVector() : size_(1), capacity_(1), container_(nullptr) {}
+    CoolerVector() : size_(0), capacity_(1), container_({}) {
+        container_.reserve(capacity_);
+    }
+
+    CoolerVector(const std::vector<T>& startVector) : size_(startVector.size()), capacity_(startVector.capacity()), container_(startVector) {}
 
     void pop_back()
     {
         try{
-            if ( size_ != 0) {
-                container_->at(size_-1) = {};
-                size_ = size_ - 1;
-            } else {
+            if ( size_ == 0) {
                 throw std::out_of_range("Cannot pop back empty vector 😡");
+            } else {
+                container_.resize(size_ - 1);
+                size_ = size_ - 1;
             }
         } catch (std::out_of_range e) {
             std::cout << "ERROR: " << e.what() << std::endl;
         }
-        
-        
     }
 
     void push_back(T value)
     {
         // ⭐TODO: Without using the prebuilt .push_back() function, finish this function so that it always removes the last element in container_
-        // 💡 Dont forget about size_ and capacity_
-        container_->push_back(value);
+        // 💡 Remember the 3 steps we just went over
     }
 
     void display()
     {
-        // for (auto value : container_)
-        // {
-        //     std::cout << value << " " << std::endl;
-        // }
+        std::cout << "CURRENT VECTOR: ";
+        for (T value : container_)
+        {
+            std::cout << value << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    T find(const T& target) {
+        // ⭐TODO: WE WILL DISCUSS THIS LATER
     }
 
 private:
     int size_;
     int capacity_;
-    std::vector<T>* container_;
+    std::vector<T> container_;
 };
